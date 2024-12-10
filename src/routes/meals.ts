b797314454a35dcfde297a45a34205a006234078
guilhermekunsch <guilherme.kunsch@outlook.com>
2024-12-10 17:49:40 -0300

    add metrics

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -3,35 +3,41 @@ import { checkSessionIdExists } from "../middlewares/check-session-id";
 import { boolean, string, z } from "zod";
 import dbKnex from "../database";
 import { randomUUID } from "crypto";
+import { user } from "./user";
+import { send } from "process";
 
 export function meals(app: FastifyInstance) {
     app.post('/', { preHandler: checkSessionIdExists }, async (request, reply) => {
-        const mealsSchema = z.object({
-            name: string().trim(),
-            description: string().trim(),
-            isOnDiet: boolean(),
-            date: z.coerce.date()
-        })
+        try {
+            const mealsSchema = z.object({
+                name: string().trim(),
+                description: string().trim(),
+                isOnDiet: boolean(),
+                date: z.coerce.date()
+            });
 
-        const { name, description, isOnDiet, date } = mealsSchema.parse(request.body)
+            const { name, description, isOnDiet, date } = mealsSchema.parse(request.body);
 
-        await dbKnex('meals').insert({
-            id: randomUUID(),
-            name,
-            description,
-            is_on_diet: isOnDiet,
-            date: date.getTime(),
-            user_id: request.user?.id
-        })
+            const mealId = randomUUID();
+            await dbKnex('meals').insert({
+                id: mealId,
+                name,
+                description,
+                is_on_diet: isOnDiet,
+                date: date.getTime(),
+                user_id: request.user?.id
+            });
 
-        return reply.code(201).send("Meals create")
-    })
+            return reply.code(201).send({ id: mealId });
+        } catch (error) {
+            return reply.code(400).send({ error: "Invalid data", details: error });
+        }
+    });
 
     app.get('/', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
-        const meals = await dbKnex("meals").where({ user_id: request.user?.id })
-        return reply.send({ meals })
-    },
-    )
+        const meals = await dbKnex("meals").where({ user_id: request.user?.id });
+        return reply.send({ meals });
+    });
 
     app.get('/:mealId', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
         try {
@@ -46,47 +52,103 @@ export function meals(app: FastifyInstance) {
 
             return reply.code(200).send(meal);
         } catch (error) {
-            console.error("Error fetching meal:", error);
-            return reply.code(500).send({ error: "Internal Server Error" });
+            return reply.code(400).send({ error: "Invalid data", details: error });
         }
     });
 
-
-    app.put("/:mealId", { preHandler: [checkSessionIdExists] }, async (request, reply) => {
-
+    app.put('/:mealId', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
         try {
-            const paramsSchema = z.object({ mealId: z.string().uuid() })
+            const paramsSchema = z.object({ mealId: z.string().uuid() });
+            const { mealId } = paramsSchema.parse(request.params);
 
-            const { mealId } = paramsSchema.parse(request.params)
-
-
-            const updateMealIdSchema = z.object({
+            const updateMealSchema = z.object({
                 name: string().trim(),
                 description: string().trim(),
                 isOnDiet: boolean(),
                 date: z.coerce.date()
-            })
+            });
 
-            const { name, description, isOnDiet, date } = updateMealIdSchema.parse(request.body)
+            const { name, description, isOnDiet, date } = updateMealSchema.parse(request.body);
 
-            const meal = dbKnex('meals').where({ id: mealId }).first()
+            const meal = await dbKnex('meals').where({ id: mealId }).first();
 
             if (!meal) {
-                return reply.code(404).send("meal not exist")
+                return reply.code(404).send({ error: "Meal not found" });
             }
 
-            await dbKnex('meals').update({
+            await dbKnex('meals').where({ id: mealId }).update({
                 name,
                 description,
                 is_on_diet: isOnDiet,
                 date: date.getTime()
-            })
+            });
 
-            return reply.code(200).send("updated meal")
+            return reply.code(200).send({ message: "Meal updated successfully" });
         } catch (error) {
-            console.error("Error fetching meal:", error);
-            return reply.code(500).send({ error: "Internal Server Error" });
+            return reply.code(400).send({ error: "Invalid data", details: error });
         }
+    });
 
-    })
+    app.delete('/:mealId', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
+        try {
+            const paramsSchema = z.object({ mealId: z.string().uuid() });
+            const { mealId } = paramsSchema.parse(request.params);
+
+            const meal = await dbKnex("meals").where({ id: mealId }).first();
+
+            if (!meal) {
+                return reply.code(404).send({ error: "Meal not found" });
+            }
+
+            await dbKnex("meals").where({ id: mealId }).delete();
+
+            return reply.code(200).send({ message: "Meal deleted successfully" });
+        } catch (error) {
+            return reply.code(400).send({ error: "Invalid data", details: error });
+        }
+    });
+
+    app.get(
+        '/metrics',
+        { preHandler: [checkSessionIdExists] },
+        async (request, reply) => {
+          const totalMealsOnDiet = await dbKnex('meals')
+            .where({ user_id: request.user?.id, is_on_diet: true })
+            .count('id', { as: 'total' })
+            .first()
+    
+          const totalMealsOffDiet = await dbKnex('meals')
+            .where({ user_id: request.user?.id, is_on_diet: false })
+            .count('id', { as: 'total' })
+            .first()
+    
+          const totalMeals = await dbKnex('meals')
+            .where({ user_id: request.user?.id })
+            .orderBy('date', 'desc')
+    
+          const { bestOnDietSequence } = totalMeals.reduce(
+            (acc, meal) => {
+              if (meal.is_on_diet) {
+                acc.currentSequence += 1
+              } else {
+                acc.currentSequence = 0
+              }
+    
+              if (acc.currentSequence > acc.bestOnDietSequence) {
+                acc.bestOnDietSequence = acc.currentSequence
+              }
+    
+              return acc
+            },
+            { bestOnDietSequence: 0, currentSequence: 0 },
+          )
+    
+          return reply.send({
+            totalMeals: totalMeals.length,
+            totalMealsOnDiet: totalMealsOnDiet?.total,
+            totalMealsOffDiet: totalMealsOffDiet?.total,
+            bestOnDietSequence,
+          })
+        },
+      )
 }
